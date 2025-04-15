137e8823f2bf069f84f693d12427e2c55b823fcc
CatShredia <sasha990g@gmail.com>
2025-04-15 08:33:04 +0300

    0.5.3 users images patch 0 init

--- project/resources/views/includes/header-tailwind.blade.php
+++ project/resources/views/includes/header-tailwind.blade.php
@@ -34,16 +34,8 @@
             <div class="flex items-center">
                 @if(Auth::check())
                     <div class="relative ml-4" x-data="{ open: false }">
-                        <button @click="open = !open" class="flex items-center text-sm rounded-full focus:outline-none">
-                            <div class="flex items-center justify-center w-8 h-8 text-white bg-gray-400 rounded-full">
-                                {{ strtoupper(substr(Auth::user()->name, 0, 1)) }}
-                            </div>
-                            <span class="ml-2 text-gray-700">{{ Auth::user()->name }}</span>
-                            <svg class="w-4 h-4 ml-1 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <button @click="open = !open">
+                            <x-logo-user></x-logo-user>
                         </button>
 
                         <!-- Выпадающее меню -->
